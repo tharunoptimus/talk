b7cd9049dc1a63c0f8ec96916d0333e253504c66
tharunoptimus <tharunoptimus@outlook.com>
2022-01-24 17:58:08 +0530

    Added faker to generate fake name if they didnt give any name

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,83 +1,74 @@
-const express = require("express");
-const keypair = require("keypair");
-const app = express();
-const router = express.Router();
-const randomize = require('randomatic');
-const cryptoString = require('crypto-string')
+const express = require("express")
+const keypair = require("keypair")
+const faker = require("faker")
+const app = express()
+const router = express.Router()
+const randomize = require("randomatic")
+const cryptoString = require("crypto-string")
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
 router.get("/keys", (_, res) => {
-    
-    let pair = keypair([64])
-    res.status(200).send(pair)
-    
+	let pair = keypair([64])
+	res.status(200).send(pair)
 })
 
 router.get("/:chatid", (req, res, next) => {
+	let payload = {
+		pageTitle: "Chat",
+		chatid: JSON.stringify(req.params.chatid),
+	}
 
-    let payload = {
-        pageTitle: "Chat",
-        chatid: JSON.stringify(req.params.chatid)
-    }
+	if (req.session.user === undefined) {
+		let randomUserId = cryptoString(10)
+		let user = {
+			_id: randomUserId,
+		}
+		payload.registered = false
+		payload.user = JSON.stringify(user)
+	} else {
+		payload.registered = true
+		payload.user = JSON.stringify(req.session.user)
+	}
 
-    if(req.session.user === undefined) {
-        let randomUserId = cryptoString(10)
-        let user = {
-            _id: randomUserId
-        }
-        payload.registered = false;
-        payload.user = JSON.stringify(user);
-    }
-    else {
-        payload.registered = true;
-        payload.user = JSON.stringify(req.session.user);
-    }
-
-    res.status(200).render("chat", payload);
+	res.status(200).render("chat", payload)
 })
 
 router.post("/create", (req, res, next) => {
+	let room = renderRoom(req.body.room)
 
-    let room = renderRoom(req.body.room);
-
-    let url = `/chat/${room}`
-    return res.redirect(url);
+	let url = `/chat/${room}`
+	return res.redirect(url)
 })
 
 router.post("/new", (req, res, next) => {
+	let user = {
+		_id: randomize("Aa0", 10),
+		name: renderName(req.body.name),
+	}
 
-    let user = {
-        _id: randomize('Aa0', 10),
-        name: renderName(req.body.name)
-    }
+	let chatid = req.body.chatid
+	chatid = chatid.replace(/[^a-zA-Z0-9]/g, "")
 
-    let chatid = req.body.chatid;
-    chatid = chatid.replace(/[^a-zA-Z0-9]/g, "");
+	let url = `/chat/${chatid}`
 
-    let url = `/chat/${chatid}`
-
-    req.session.user = user;
-    return res.redirect(url);
-    
-
+	req.session.user = user
+	return res.redirect(url)
 })
 
-function renderRoom (string) {
-    if(string.length < 10 && /^[a-zA-Z0-9 ]+$/.test(string)) {
-        return string.replace(/\s/g, "");
-    }
-    return cryptoString(10)
+function renderRoom(string) {
+	if (string.length < 10 && /^[a-zA-Z0-9 ]+$/.test(string)) {
+		return string.replace(/\s/g, "")
+	}
+	return cryptoString(10)
 }
 
 function renderName(string) {
-    if(string.length < 20 && /^[a-zA-Z0-9 ]+$/.test(string)) {
-        return string;
-    }
-    return "Anonymous";
+	if (string.length < 20 && /^[a-zA-Z0-9 ]+$/.test(string)) {
+		return string
+	}
+	return faker.name.findName()
 }
 
-
-module.exports = router; 
+module.exports = router
